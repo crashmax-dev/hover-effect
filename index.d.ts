b409fea5c5ccd4b07bc316117b45ae4a87132025
crashmax-dev <crashmax.dev@gmail.com>
2022-04-03 17:26:33 +0000

    Deploying to gh-pages from @ crashmax-dev/hover-effect@75d5928b7a413e044fbc81e47dca30a0e4ffb51f 🚀

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare class HoverEffect {
+    private el;
+    private threshold;
+    constructor(el: HTMLDivElement);
+    private mouseenter;
+    private mouseleave;
+    private mousemove;
+}
+declare const motion: MediaQueryList;
+declare const card: HTMLDivElement;
