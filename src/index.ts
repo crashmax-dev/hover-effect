220124b44d0e35530f7069a15fc6e33bca98f718
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-04-04 01:23:30 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,48 @@
-interface IUser {
-  firstName: string
-  lastName: string
-  age: number
+class HoverEffect {
+  private el: HTMLDivElement
+  private threshold = 20
+
+  constructor(el: HTMLDivElement) {
+    this.el = el
+    this.el.addEventListener('mouseenter', this.mouseenter.bind(this))
+    this.el.addEventListener('mouseleave', this.mouseleave.bind(this))
+    this.el.addEventListener('mousemove', this.mousemove.bind(this))
+  }
+
+  private mouseenter(event: MouseEvent): void {
+    this.el.classList.add('over')
+  }
+
+  private mouseleave(event: MouseEvent): void {
+    this.el.classList.remove('over')
+    this.el.style.transform = `perspective(${this.el.clientWidth}px) rotateX(0deg) rotateY(0deg)`
+  }
+
+  private mousemove(event: MouseEvent) {
+    const { clientX, clientY, currentTarget } = event
+    const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget as HTMLDivElement
+
+    const horizontal = (clientX - offsetLeft) / clientWidth
+    const vertical = (clientY - offsetTop) / clientHeight
+    const rotateY = (vertical * this.threshold - this.threshold / 2).toFixed(2)
+    const rotateX = (this.threshold / 2 - horizontal * this.threshold).toFixed(2)
+
+    // const _rotateY = (vertical * this.threshold - this.threshold / 2).toFixed(2)
+    // const _rotateX = (horizontal * this.threshold - this.threshold / 2).toFixed(2)
+
+    let transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg)`
+
+    if (this.el.classList.contains('over')) {
+      transform += ' scale3d(1.07, 1.07, 1.07)'
+    }
+
+    card.style.transform = transform
+  }
 }
 
-export class User {
-  private firstName: string
-  private lastName: string
-  private age: number
+const motion = window.matchMedia('(prefers-reduced-motion)')
+const card = document.querySelector<HTMLDivElement>('.card')!
 
-  constructor({ firstName, lastName, age }: IUser) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-  }
-
-  get name(): string {
-    return `${this.firstName} ${this.lastName}`
-  }
-
-  isOverEighteen(): boolean {
-    return this.age >= 18
-  }
+if (!motion.matches) {
+  new HoverEffect(card)
 }
